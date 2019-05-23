7cc3043060078664bb66eaceccedaaa16b73bd98
Nick Sadler <nicksadler14@gmail.com>
2019-05-22 22:52:17 -0400

    csv into dataframes

--- Main.py
+++ Main.py
@@ -1,1 +1,11 @@
-print("Hello git!")
+import pandas as pd
+
+def main():
+    test_data = pd.read_csv('test.csv', header=0)
+    train_data = pd.read_csv('train.csv', header=0)
+    
+    
+def clean_data(data):
+    return data
+    
+main()
